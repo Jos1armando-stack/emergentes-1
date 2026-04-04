924715d9492cee19911b644f2f98e69eb4d60ba6
armandacho <armandoayaarmando@gmail.com>
2026-04-04 10:35:22 -0400

    Se agrega la Segunda Practica

---
+++ tareaN1-emergentes/src/main/java/emergentes/tareaN1/emergentes/Hello.java
@@ -0,0 +1,27 @@
+package emergentes.tareaN1.emergentes;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+@RestController
+public class Hello {
+    ArrayList<String> lista = new ArrayList<>();
+
+    @GetMapping("/hello")
+    public String GetHello() {
+        String text = "";
+        for (int i = 0; i < lista.size(); i++) {
+            text = text + lista.get(i);
+        }
+        return text;
+    }
+
+    @PostMapping("/hello")
+    public String PostHello(@RequestBody String variable){
+        lista.add(variable);
+        return variable;
+    }
+}
